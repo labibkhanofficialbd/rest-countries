5dc6c40b1df3e947f407d87e38fe619dae9dafe5
labibkhanofficialbd <labibkhan66410@gmail.com>
2021-09-23 18:52:00 +0600

    all countries added

--- js/countries.js
+++ js/countries.js
@@ -5,6 +5,19 @@ const loadCountries = () => {
 }
 
 const displayCountries = countries => {
-    console.log(countries);
+    console.log(countries[0]);
+    const container = document.getElementById('countries');
+    const countryHTML = countries.map(country => getCountryHTML(country));
+    container.innerHTML = countryHTML.join(' ');
+    console.log(countryHTML[0]);
+}
+const getCountryHTML = country => {
+    return `
+        <div class="country">
+            <h2>${country.name}</h2>
+            <h4>${country.capital}</h4>
+            <img src="${country.flags[1]}">
+        </div>
+    `
 }
 loadCountries();
